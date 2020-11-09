2e3fd4ef31043be96f59f43a219e84b018d0b72f
hossam-khalaf <hossam.khalaf@med.au.edu.eg>
2020-11-09 02:40:55 +0200

    cleaner

--- src/Counter.js
+++ src/Counter.js
@@ -1,20 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Counter = () => {
-  const [count, setCount] = React.useState(0);
-
-  const handleIncrement = () => {
-    setCount(count + 1);
-  };
-  const handleDecrement = () => {
-    setCount(count - 1);
-  };
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <p>The Count Is: {count} </p>
-      <button onClick={handleIncrement}>Increment</button>
-      <button onClick={handleDecrement}>Decrement</button>
+      <div>
+        <button onClick={() => setCount(count - 1)}> - </button>
+        <button onClick={() => setCount(count + 1)}> + </button>
+      </div>
     </>
   );
 };
